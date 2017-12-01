7e2d5fcfa1d51cde6bcc39e8db06c130b682a89a
vulcansparkle <32465403+vulcansparkle@users.noreply.github.com>
2017-12-01 14:16:06 -0500

    Update Easy.java

--- Easy.java
+++ Easy.java
@@ -11,4 +11,35 @@ package suffersquad;
  */
 public class Easy extends Dragon{
     
+    import java.util.Random;
+
+public class EasyDragon extends Dragon {
+    
+    Random rand = new Random();
+
+    public EasyDragon(int dlevel, String dname, treasure dtype) {
+        super(dlevel, dname, dtype);
+        
+       
+    }
+    
+    @Override
+    public int attack(int opponent){
+        
+        int power = rand.nextInt(20)+1;
+        
+        if((power >= opponent)&& power != opponent) {
+            
+            return 1;
+            
+        } 
+        else {
+            
+            return 0;
+        }
+    }
+    
+    
+}
+    
 }
