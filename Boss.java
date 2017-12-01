2cd5c5f4e862a971339e756c0693f08540e7d437
vulcansparkle <32465403+vulcansparkle@users.noreply.github.com>
2017-12-01 14:17:41 -0500

    Update Boss.java

--- Boss.java
+++ Boss.java
@@ -12,4 +12,33 @@ package dragondungeongame;
  */
 public class Boss extends Dragon{
     
+    Random rand = new Random();
+
+    public Bo$$Dragon(int dlevel, String dname, treasure dtype) {
+        super(dlevel, dname, dtype);
+        
+       
+    }
+    
+    @Override
+    public int attack(int opponent){
+        
+        int power = rand.nextInt(75)+1;
+        
+        if((power >= opponent)&& power != opponent) {
+            
+            return 0;
+            
+        } 
+        else if(power + 10 <= opponent){
+            return 1;
+        }
+        else {            
+            return 0;
+        }
+    }
+    
+    
+
+
 }
