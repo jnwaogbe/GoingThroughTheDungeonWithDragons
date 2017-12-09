c7f455b0a6667da105c04f3e59c416c370ac0e09
sjacobson456 <sjacobson28@gmail.com>
2017-12-09 00:13:52 -0500

    Update Treasure.java
    treasure class out here

--- src/dragondungeongame/Treasure.java
+++ src/dragondungeongame/Treasure.java
@@ -11,4 +11,38 @@ package dragondungeongame;
  */
 public class Treasure {
     
+    String type;
+    
+    public Treasure(String dType){
+        type = dType;
+    }
+    
+    
+    public void PowerUp(Player player){
+        
+        type.toLowerCase();
+        
+        if(type == "sleep"){
+            System.out.println("Congrats! You got some sleep. Your health has been improved.");
+            player.health = player.health + 10;
+        }
+        
+        if(type == "food"){
+            System.out.println("Congrats! You got some food! Your health has been improved.");
+            player.health = player.health +15;
+        }
+        
+        if(type == "treasure"){
+            System.out.println("Congrats! You found some treasure! Your attack has been increased.");
+            player.attackPower = player.attackPower + 10;
+        }
+        
+        
+        
+    }
 }
+    
+            
+            
+            
+  
