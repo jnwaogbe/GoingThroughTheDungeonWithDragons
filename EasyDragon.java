6e7d3b302db82c33342c47ca1336fc83d924c84c
vulcansparkle <32465403+vulcansparkle@users.noreply.github.com>
2017-12-09 01:02:59 -0500

    Add files via upload
    Added boolean to Easy!

---
+++ EasyDragon.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dragondungeongame;
+
+/**
+ *
+ * @author Me
+ */
+public class EasyDragon extends Dragon{
+    
+    boolean defeated = false;
+    boolean friend = false;
+    
+    
+    
+
+    public EasyDragon( String dname) {
+        super(dname);
+        this.health = 10;
+        this.attackPower = 5;
+        this.defense = 6;
+        
+    }
+    
+    
+    
+     
+    @Override
+   public String getDifficulty(){
+       
+       return "easy";
+   
+   }
+   
+ 
+   
+  
+    
+    @Override
+    public int attack(int playerDefense) {
+              
+     int hit;        
+        int attackNum = rand.nextInt(10)+5;
+        
+        if(attackNum > playerDefense){
+            hit = attackNum - playerDefense;
+            
+        }
+        else{
+            hit = 0;
+        }
+        
+        return hit;
+    
+    
+    }
+}
