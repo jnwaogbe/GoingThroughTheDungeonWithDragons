31481e9890d6670c58c135422610fc04fa323855
sjacobson456 <sjacobson28@gmail.com>
2017-12-08 19:22:53 -0500

    Update TreasureTest.java

--- test/dragondungeongame/TreasureTest.java
+++ test/dragondungeongame/TreasureTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package suffersquad;
+package dragondungeongame;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
